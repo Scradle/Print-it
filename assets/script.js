f696ac672fc62ead5e944cea75ee9bf5538c8695
Scradle <152749477+Scradle@users.noreply.github.com>
2024-02-13 02:53:15 +0100

    ajout des flêches et des bullets

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,48 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+//Bloc flêches
+const Arrow = document.querySelector('.arrow');
+Arrow.style.setProperty('display', 'flex');
+Arrow.style.setProperty('flex-direction', 'row');
+
+//flêche gauche
+const ArrowLeft = document.getElementById("arrow_left");
+ArrowLeft.addEventListener('click', function(event) {
+    // Différenciez le clic gauche du clic droit
+    if (event.button === 0) {
+        console.log("Clic gauche sur la flèche gauche");
+    } else if (event.button === 2) {
+        console.log("Clic droit sur la flèche gauche");
+    }
+});
+
+//flêche droite
+const ArrowRight = document.getElementById("arrow_right");
+ArrowRight.addEventListener('click', function(event) {
+    // Différenciez le clic gauche du clic droit
+    if (event.button === 0) {
+        console.log("Clic gauche sur la flèche droite");
+    } else if (event.button === 2) {
+        console.log("Clic droit sur la flèche droite");
+    }
+});
+
+//nombre de dots grâce au nombre de slides du tableau slides
+const nombreSlides = slides.length;
+console.log("Nombre d'éléments dans le tableau :", nombreSlides);
+
+for (let i = 0; i < nombreSlides; i++) {
+
+	let Dot = document.createElement("div");
+    // Récupérer un élément parent existant
+	let Dots = document.getElementById("dots");
+
+	// Ajouter le nouvel élément au parent
+	Dots.appendChild(Dot);
+	//ajout de classe sur élément crée
+	Dot.classList.add("dot");
+
+}
